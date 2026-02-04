76003a86b2e594b5edcd6d5203a4955f80bdd220
brianho123 <hobrian71@gmail.com>
2026-02-05 01:32:15 +0800

    Configured product selection and filtering

--- src/components/ProductGrid.tsx
+++ src/components/ProductGrid.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import { Product } from '@/types/product'
 import { useRouter, useSearchParams } from 'next/navigation'
+import { categories } from '@/data/products';
 
 interface ProductGridProps {
     products: Product[];
@@ -18,9 +19,35 @@ const ProductGrid = ({products, initialCategory = 'all'}: ProductGridProps) => {
     setSelectedCategory(category);
   }, [searchParams])
 
+  const handleCategoryChange = (categorySlug: string) => {
+    setSelectedCategory(categorySlug);
+    const params = new URLSearchParams(searchParams.toString());
+    if (categorySlug === 'all') {
+      params.delete('category');
+    }
+    else {
+      params.set('category', categorySlug);
+    }
+    router.push(`/?${params.toString()}`, {scroll: false});
+  }
+
   const filteredProducts = selectedCategory === 'all' ? products : products.filter(product => product.category === selectedCategory);
+  console.log(filteredProducts);
+
   return (
-    <div>ProductGrid</div>
+    <div>
+      <div className='mb-8 flex flex-wrap gap-4 border-b border-gray-200 pb-4'>
+        {
+          categories.map((category) =>
+            <button key={category.slug}
+            onClick={() => handleCategoryChange(category.slug)}
+            className={`font-medium transition-colors cursor-pointer ${selectedCategory === category.slug ? 'text-indigo-600' : ''}`}
+            >{category.name}</button>          
+          )
+
+        }
+      </div>
+    </div>
   )
 }
 
